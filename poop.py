f64972a5d5c45406d74c3e5fcfb7fed8f9bb320c
Zeph592 <82102223+Zeph592@users.noreply.github.com>
2021-04-21 09:48:21 -0400

    hungry

--- poop.py
+++ poop.py
@@ -305,7 +305,7 @@ def flashBang():
 	all_Off()
 def pacman(): #fix speed and color
 	delay=0
-	pixels.fill((255,240,0))
+	pixels.fill((168,227,255))
 	for i in range(LED_COUNT):
 		pixels[i]=(255,255,0)
 		if i >=1:
