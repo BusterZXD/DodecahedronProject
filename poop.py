38e93230a030290c3f9ab424b394e5166dafece9
Zeph592 <82102223+Zeph592@users.noreply.github.com>
2021-04-28 09:15:04 -0400

    ok

--- poop.py
+++ poop.py
@@ -305,8 +305,10 @@ def pacmanUpdated():
 	for j in range(LED_COUNT):
 		pixels[j]=(255,255,0)
 		if j>=1:
+			print("OFF -1")
 			pixels[i-1]=(0,0,0)
 		elif j==0:
+			print("OFF -2")
 			pixels[i]=(0,0,0)
 		elif pixels[i]==(255,255,0):
 			coin=random.randint(0,1)
